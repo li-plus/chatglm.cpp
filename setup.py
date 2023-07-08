f0433b49d9d6b8fcc6b82c000cad4fafe8c4a461
Jiahao Li <liplus17@163.com>
2023-07-08 12:17:17 +0800

    Prepare for python package release (#42)
    * Use pyproject.toml for project packaging
    
    * Adjust MANIFEST to reduce sdist size
    
    * Add python release action

--- setup.py
+++ setup.py
@@ -30,6 +30,9 @@ class CMakeBuild(build_ext):
             f"-DCHATGLM_ENABLE_PYBIND=ON",
         ]
 
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
         build_temp = Path(self.build_temp) / ext.name
         build_temp.mkdir(parents=True, exist_ok=True)
 
@@ -38,30 +41,11 @@ class CMakeBuild(build_ext):
 
 
 HERE = Path(__file__).resolve().parent
-version = re.search(r'__version__ = "(.*?)"', (HERE / "chatglm_cpp/__init__.py").read_text()).group(1)
+version = re.search(r'__version__ = "(.*?)"', (HERE / "chatglm_cpp/__init__.py").read_text(encoding="utf-8")).group(1)
 
 setup(
-    name="chatglm-cpp",
     version=version,
-    author="Jiahao Li",
-    author_email="liplus17@163.com",
-    maintainer="Jiahao Li",
-    maintainer_email="liplus17@163.com",
-    url="https://github.com/li-plus/chatglm.cpp",
-    description="C++ implementation of ChatGLM-6B",
-    long_description=(HERE / "README.md").read_text(),
-    long_description_content_type="text/markdown",
     packages=find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-    ],
-    keywords=["chatglm", "large language model"],
-    license="MIT",
-    python_requires=">=3.7",
     ext_modules=[CMakeExtension("chatglm_cpp._C")],
     cmdclass={"build_ext": CMakeBuild},
 )
