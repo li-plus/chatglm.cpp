747259a86f534c32e4873d31b377f080807ab6de
Mike Liu (ML) <88017282+emikeliu@users.noreply.github.com>
2023-08-05 22:16:07 +0800

    web_demo.py bug fixed (#19)
    Now web_demo.py won't display HTML tags on gradio chatbot.

--- examples/web_demo.py
+++ examples/web_demo.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 import chatglm_cpp
 import gradio as gr
-import mdtex2html
 
 DEFAULT_MODEL_PATH = Path(__file__).resolve().parent.parent / "chatglm-ggml.bin"
 
@@ -24,8 +23,8 @@ def postprocess(self, y):
         return []
     for i, (message, response) in enumerate(y):
         y[i] = (
-            None if message is None else mdtex2html.convert((message)),
-            None if response is None else mdtex2html.convert(response),
+            None if message is None else ((message)),
+            None if response is None else (response),
         )
     return y
 
@@ -34,35 +33,16 @@ gr.Chatbot.postprocess = postprocess
 
 
 def parse_text(text):
-    """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
     lines = text.split("\n")
     lines = [line for line in lines if line != ""]
-    count = 0
+    flag = True
     for i, line in enumerate(lines):
         if "```" in line:
-            count += 1
-            items = line.split("`")
-            if count % 2 == 1:
-                lines[i] = f'<pre><code class="language-{items[-1]}">'
-            else:
-                lines[i] = f"<br></code></pre>"
+            flag = not flag
         else:
             if i > 0:
-                if count % 2 == 1:
-                    line = line.replace("`", "\`")
-                    line = line.replace("<", "&lt;")
-                    line = line.replace(">", "&gt;")
-                    line = line.replace(" ", "&nbsp;")
-                    line = line.replace("*", "&ast;")
-                    line = line.replace("_", "&lowbar;")
-                    line = line.replace("-", "&#45;")
-                    line = line.replace(".", "&#46;")
-                    line = line.replace("!", "&#33;")
-                    line = line.replace("(", "&#40;")
-                    line = line.replace(")", "&#41;")
-                    line = line.replace("$", "&#36;")
-                lines[i] = "<br>" + line
-    text = "".join(lines)
+                lines[i] = line
+    text = "\n".join(lines)
     return text
 
 
