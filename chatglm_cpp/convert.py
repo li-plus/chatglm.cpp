b2d89c6feddac2ad2b456b95a16a5a10af3c8e82
Jiahao Li <liplus17@163.com>
2024-03-06 20:51:46 +0800

    Fix missing lora path argument in convert script (#271)
    * Fix missing lora path argument
    
    * Format code with latest black

--- chatglm_cpp/convert.py
+++ chatglm_cpp/convert.py
@@ -1,6 +1,7 @@
 """
 Convert Hugging Face ChatGLM/ChatGLM2 models to GGML format
 """
+
 import argparse
 import platform
 import struct
@@ -534,7 +535,7 @@ def main():
     args = parser.parse_args()
 
     with open(args.save_path, "wb") as f:
-        convert(f, args.model_name_or_path, dtype=args.type)
+        convert(f, args.model_name_or_path, args.lora_model_name_or_path, dtype=args.type)
 
     print(f"GGML model saved to {args.save_path}")
 
